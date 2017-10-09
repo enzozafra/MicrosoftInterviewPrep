646907c0460028322f78429daac597d4178bdf31
enzozafra <zafra@ualberta.ca>
2017-10-09 16:12:50 -0600

    Start practice questions

--- PracticeQuestions/ArrayAndStringsPractice.java
+++ PracticeQuestions/ArrayAndStringsPractice.java
@@ -73,9 +73,163 @@ class ArrayAndStringsPractice {
     return true;
   }
 
+  public static void urlify(char[] str, int trueLength) {
+    int spaceCount = 0, index;
+    for (int i = 0; i < trueLength; i++) {
+      if (str[i] == ' ') {
+        spaceCount++;
+      }
+    }
+    index = trueLength + spaceCount * 2; // need to triple the space value
+    if (trueLength < str.length) {
+      // end of array
+      str[trueLength] = '\0';
+    }
+    for (int i = trueLength - 1; i >= 0; i--) {
+      if(str[i] == ' ') {
+        str[index - 1] = '0';
+        str[index - 2] = '2';
+        str[index - 3] = '%';
+        index = index - 3;
+      } else {
+        str[index - 1] = str[i];
+        index --;
+      }
+    }
+  }
+
+  public static boolean palin_perm(String input) {
+    input = input.toLowerCase();
+    int[] count = new int[128];
+    for (int i = 0; i < input.length(); i++) {
+      if(input.charAt(i) == ' ') {
+        continue;
+      }
+      else {
+        count[input.charAt(i)]++;
+      }
+    }
+    int counter = 0;
+    for (int j = 0; j < count.length - 1; j++) {
+      if (count[j] % 2 != 0) {
+        counter++;
+      }
+    }
+    if (counter > 1) {
+      return false;
+    }
+    return true;
+  }
+
+  static boolean oneEditReplace(String s1, String s2) {
+    boolean foundDifference = false;
+    for (int i = 0; i < s1.length(); i++) {
+      if (s1.charAt(i) != s2.charAt(i)) {
+        if (foundDifference) {
+          return false;
+        }
+        foundDifference = true;
+      }
+    }
+    return true;
+  }
+
+  static boolean oneEditInsert(String s1, String s2) {
+    int index1 = 0;
+    int index2 = 0;
+    while (index2 < s2.length() && index1 < s1.length()) {
+      if (s1.charAt(index1) != s2.charAt(index2)) {
+        if (index1 != index2) {
+          return false;
+        }
+        index2++;
+      } else {
+        index1++;
+        index2++;
+      }
+    }
+    return true;
+  }
+
+  public static String compression(String s1) {
+    if (s1 == "") {
+      return s1;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    char prev = s1.charAt(0);
+
+    for (int i = 0; i < s1.length(); i++) {
+      char item = s1.charAt(i);
+      if (item == prev) {
+        count++;
+      } else {
+        sb.append(prev);
+        sb.append(String.valueOf(count));
+        count = 1;
+      }
+      prev = item;
+    }
+    sb.append(prev);
+    sb.append(String.valueOf(count));
+    return sb.length() < s1.length() ? sb.toString() : s1;
+  }
+
+  // Cleaner solution to compression also check length first to avoid SB time.
+  public static String compression_better(String s1) {
+    // check final length and return input string if it would be longer
+    int finalLength = countCompression(s1);
+    if (finalLength >= s1.length()) return s1;
+
+    StringBuilder compressed = new StringBuilder();
+    int count = 0;
+    for (int i = 0; i < s1.length(); i++) {
+      count++;
+
+      // if next is different from current, append to SB.
+      if (i + 1 >= s1.length() || s1.charAt(i) != s1.charAt(i+1)) {
+        compressed.append(s1.charAt(i));
+        compressed.append(count);
+        count = 0;
+      }
+    }
+    return compressed.toString();
+  }
+
+  public static int countCompression(String s1) {
+    int compressedLength = 0;
+    int count = 0;
+    for (int i = 0; i < s1.length(); i++) {
+      count++;
+
+      // if next is different from current, append to SB.
+      if (i + 1 >= s1.length() || s1.charAt(i) != s1.charAt(i+1)) {
+        compressedLength += 1 + String.valueOf(count).length();
+        count = 0;
+      }
+    }
+    return compressedLength;
+  }
+
+  public static boolean oneaway(String first, String second) {
+    if (first.length() == second.length()) {
+      return oneEditReplace(first, second);
+    } else if (first.length() + 1 == second.length()) {
+      return oneEditInsert(first, second);
+    } else if (first.length() - 1 == second.length()) {
+      return oneEditInsert(second, first);
+    }
+    return false;
+  }
+
+
   public static void main(String [] args) {
-    testUnique();
-    testPermutation();
+    // testUnique();
+    // testPermutation();
+    // testurlify();
+    // testpalindrome();
+    test_compression();
   }
 
   public static void testUnique() {
@@ -175,5 +329,96 @@ class ArrayAndStringsPractice {
     } else {
       System.out.println("String1: " + test7_s1 + " String2: " + test7_s2 + " is NOT permutation");
     }
+  }
+
+  public static void testurlify() {
+    char[] test = "I am a normal string".toCharArray();
+    urlify(test, test.length);
+    System.out.println(test);
+
+    test = "I am a string with spaces in the end    ".toCharArray();
+    urlify(test, test.length - 4);
+    System.out.println(test);
+
+    test = "     I am a string with spaces in the beginning".toCharArray();
+    urlify(test, test.length - 5);
+    System.out.println(test);
+
+    test = "I     am     a string with spaces      in the middle".toCharArray();
+    urlify(test, test.length - 13);
+    System.out.println(test);
+
+  }
+
+  public static void testpalindrome() {
+    String input = "Tact Coa";
+    if (palin_perm(input)) {
+      System.out.println(input + " is true!");
+    } else {
+      System.out.println(input + " is false :(");
+    }
+
+    String input1 = "Tact                     Coa";
+    if (palin_perm(input1)) {
+      System.out.println(input1 + " is true!");
+    } else {
+      System.out.println(input1 + " is false :(");
+    }
+
+    String input2 = "Tact Coa     ";
+    if (palin_perm(input2)) {
+      System.out.println(input2 + " is true!");
+    } else {
+      System.out.println(input2 + " is false :(");
+    }
+
+    String input3 = "            Tact Coa";
+    if (palin_perm(input3)) {
+      System.out.println(input3 + " is true!");
+    } else {
+      System.out.println(input3 + " is false :(");
+    }
+
+    String input4 = "";
+    if (palin_perm(input4)) {
+      System.out.println(input4 + " is true!");
+    } else {
+      System.out.println(input4 + " is false :(");
+    }
+
+    String input5 = "TactCoa";
+    if (palin_perm(input5)) {
+      System.out.println(input5 + " is true!");
+    } else {
+      System.out.println(input5 + " is false :(");
+    }
+
+    String input6 = "Tact Coat";
+    if (palin_perm(input6)) {
+      System.out.println(input6 + " is true!");
+    } else {
+      System.out.println(input6 + " is false :(");
+    }
+
+    String input7 = "Taactt Coaccat";
+    if (palin_perm(input7)) {
+      System.out.println(input7 + " is true!");
+    } else {
+      System.out.println(input7 + " is false :(");
+    }
+  }
+
+  public static void test_compression() {
+    String test1 = "aabcccccaaa";
+    String test2 = "abcdefghijklmnop";
+    String test3 = "aaaaaaaaaa";
+    String test4 = "";
+    String test5 = "aabb";
+
+    System.out.println("Uncompressed: " + test1 + " compressed: " + compression(test1));
+    System.out.println("Uncompressed: " + test2 + " compressed: " + compression(test2));
+    System.out.println("Uncompressed: " + test3 + " compressed: " + compression(test3));
+    System.out.println("Uncompressed: " + test4 + " compressed: " + compression(test4));
+    System.out.println("Uncompressed: " + test5 + " compressed: " + compression(test5));
   }
 }
---
+++ PracticeQuestions/LinkedListPractice.java
@@ -0,0 +1,190 @@
+import java.io.*;
+import java.util.*;
+
+public class LinkedListPractice {
+
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node () {
+      this.next = null;
+    }
+
+    public Node (int d) {
+      this.data = d;
+      this.next = null;
+    }
+  }
+
+  static Node head;
+
+  static void removedups(Node n) {
+    HashSet<Integer> list = new HashSet<Integer>();
+    Node previous = n;
+    while (n != null) {
+      if (list.contains(n.data)) {
+        previous.next = n.next;
+        n = n.next;
+      }
+      else {
+        list.add(n.data);
+        previous = n;
+      }
+    }
+    n = n.next;
+  }
+
+  static Node kthtolast(Node head, int k) {
+    Node p1 = head;
+    Node p2 = head;
+    for (int i = 0; i < k; i++) {
+      if (p1 == null) return null;  // out of bounds
+      p1 = p1.next;
+    }
+    // Move at same pace, when p1 is at end, then p2 will be at k'th last element
+    while (p1 != null) {
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    return p2;
+  }
+
+  static Node kthtolast_recur(Node head, int k, int[] counter) {
+    if (head == null) {
+      return null;
+    }
+    Node node = kthtolast_recur(head.next, k, counter);
+    counter[0] = counter[0] + 1;
+    if (counter[0] == k) {
+      return head;
+    }
+    return node;
+  }
+
+  static void deletemiddle(Node tobedeleted) {
+    if (tobedeleted == null || tobedeleted.next == null) {
+      return;
+    }
+    tobedeleted.data = tobedeleted.next.data;
+    tobedeleted.next = tobedeleted.next.next;
+  }
+
+  static Node partition(int partvalue) {
+    Node n = head;
+    Node list1 = head;
+    Node list1head = list1;
+    Node list2 = head;
+    Node list2head = list2;
+
+    while (n != null) {
+      Node next = n.next;
+      n.next = null;
+      if (n.data < partvalue) {
+        list1.next = n;
+        list1 = n;
+      } else {
+        list2.next = n;
+        list2 = n;
+      }
+      n = next;
+    }
+    list2.next = null;
+    list1.next = list2head.next;
+    return list1head.next;
+  }
+
+  public static int sumlists(Node n1, Node n2) {
+    int num1 = 0;
+    int num2 = 0;
+
+    int multiplier = 1;
+    while(n1 != null) {
+      num1 += n1.data * multiplier;
+      multiplier = multiplier*10;
+      n1 = n1.next;
+      System.out.println(String.valueOf(num1));
+    }
+
+    multiplier = 1;
+    while(n2 != null) {
+      num2 += n2.data * multiplier;
+      multiplier = multiplier*10;
+      n2 = n2.next;
+      System.out.println(String.valueOf(num2));
+    }
+
+    int out = num1 + num2;
+    return out;
+
+  }
+
+  public static Node sumlists_recur(Node n1, Node n2, int carry) {
+    if(n1 == null && n2 == null && carry == 0) {
+      return null;
+    }
+
+    Node result = new Node(-1);
+    int value = carry;
+    if (n1 != null) {
+      value += n1.data;
+    }
+    if (n2 != null) {
+      value += n2.data;
+    }
+
+    result.data = value % 10; // get ones digit of number
+
+    // Recurse
+    if (n1 != null || n2 != null) {
+      Node more = sumlists_recur(n1 == null ? null : n1.next,
+                                n2 == null ? null : n2.next,
+                                value >= 10 ? 1 : 0);
+      result.next = more;
+    }
+    return result;
+  }
+
+  // traversal, print list
+  public static void printList (Node x) {
+    Node n = x;
+    while (n != null) {
+      System.out.print(n.data + " ");
+      n = n.next;
+    }
+  }
+
+  public static Node insertNode(Node n, int d) {
+    Node newNode = new Node(d);
+    newNode.next = n;
+    n = newNode;
+    return n;
+  }
+
+  public static void main(String [] args) {
+    testsum();
+  }
+
+  public static void testsum() {
+    Node n1 = new Node(6);
+    n1 = insertNode(n1, 1);
+    n1 = insertNode(n1, 7);
+
+    Node n2 = new Node(2);
+    n2 = insertNode(n2, 9);
+    n2 = insertNode(n2, 5);
+
+    printList(sumlists_recur(n1, n2, 0));
+    System.out.println("");
+
+
+    n1 = new Node(6);
+    n1 = insertNode(n1, 1);
+
+    n2 = new Node(2);
+    n2 = insertNode(n2, 9);
+    n2 = insertNode(n2, 5);
+
+    printList(sumlists_recur(n1, n2, 0));
+  }
+}
